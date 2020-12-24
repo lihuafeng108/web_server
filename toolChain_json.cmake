4c26e2527e8b477bf748536802ead10d4b8f0ae3
lihf <lihf@genvict.com>
2020-12-24 15:00:19 +0800

    添加子模块libubox，文档目录名称改为doc，新增两个cmake编译文件，分别对应json-c和libubox

---
+++ toolChain_json.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME Linux)
+ 
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+ 
+set(CMAKE_FIND_ROOT_PATH "/mnt/share/src/web_server/json-c")
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
---
+++ toolChain_libubox.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+ 
+set(INSTALL_PATH "/mnt/share/build")
+include_directories(${INSTALL_PATH}/include)
+include_directories(${INSTALL_PATH}/include/json-c)
+ 
+set(CMAKE_FIND_ROOT_PATH "/mnt/share/src/web_server/libubox")
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+ 
+set(json "/mnt/share/build/lib/libjson-c.so")
