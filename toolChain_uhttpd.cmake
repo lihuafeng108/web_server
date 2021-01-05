c18acf317b14fc721450c896ac476b4123fcfca2
lihf <lihf@genvict.com>
2021-01-05 15:40:16 +0800

    修改uhttpd的编译问题

--- toolChain_uhttpd.cmake
+++ toolChain_uhttpd.cmake
@@ -11,7 +11,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
  
 set(blob_library     ${INSTALL_PATH}/lib/libblobmsg_json.so)
-set(json             ${INSTALL_PATH}/lib/libjson-c.so)
+set(libjson             ${INSTALL_PATH}/lib/libjson-c.so)
 set(ubox_include_dir ${INSTALL_PATH}/include/libubox)
 set(ubox_library     ${INSTALL_PATH}/lib/libubox.so)
 set(ubus             ${INSTALL_PATH}/lib/libubus.so)
